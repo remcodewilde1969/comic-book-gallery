7b2ad1d0f0c2f11727b602c1c989948954d7adae
Remco de Wilde <remcodewilde@gmail.com>
2017-05-10 09:34:23 +0200

    Added Detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Dit is mijn eerste MCV Controller Method";
+        }
     }
 }
